957b6ead2af1ea57cca0c4141be64c8ed1079c8d
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-13 20:55:56 -0700

    Done, task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- Serves as the entry of a program
+ * Return: Returns 1 if true else a zero value
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
