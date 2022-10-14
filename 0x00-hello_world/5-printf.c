71fa86ed6b27ceb936e8b7455f2983a3dbfd2918
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-13 17:48:36 -0700

    Done, task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point of the program
+ * Description- print "with proper grammar, but the outcome is a piece of art,"
+ * Return- Return is zero if  no error else return non-zero value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
