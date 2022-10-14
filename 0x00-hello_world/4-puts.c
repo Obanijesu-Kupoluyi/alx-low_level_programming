235c4206d66459edf53d277868d44a75d9bb3ab6
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-13 17:25:00 -0700

    Updated, task 4 at last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - this function block start
- * Description - prints \"Programming is like building a miltilingual puzzle
- * Return - always return 0
+/**
+ * main- Serves as the entry point fot the program
+ * Description- prints \"Programming is like building a miltilingual puzzle
+ * Return: Returns zero if no errors otherwise returns non-zero value
  */
 int main(void)
 {
