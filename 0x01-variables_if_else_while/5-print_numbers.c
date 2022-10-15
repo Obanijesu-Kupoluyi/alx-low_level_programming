423ab751d1c891b0b79fc618b1737e85f91f143b
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-14 20:09:51 -0700

    Done, task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- Serves as the main entry of the program
+ * Return: return is zero, non-zero value if any error
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
