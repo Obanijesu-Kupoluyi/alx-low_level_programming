b0eabbf0135b9e88d9c3c87af0b6135b7f081a4e
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-14 21:08:04 -0700

    Done, task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- Serves as the entry point of the program
+ * Return: zero
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
