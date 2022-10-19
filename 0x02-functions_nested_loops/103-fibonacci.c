d417103fb6bcfeb31807957f2ffff35c7529f96c
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-18 21:43:12 -0700

    Done task 13 - 16

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: zero
+ */
+int main(void)
+{
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
