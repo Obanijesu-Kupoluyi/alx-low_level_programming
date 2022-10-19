c56772ea7ee0e67b0d0ee4bf8d12864ecac07c67
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-18 17:43:17 -0700

    Done task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@
 void print_alphabet_x10(void)
 {
 	char n;
+	int y, z;
 
 	for (n = 'a'; n <= 'z'; n++)
 	{
-		for (int y = '0'; y <= 10; y++)
+		for (y = '0'; y <= 10; y++)
 		{
-			for (int z = '0'; z <= y; z++)
+			for (z = '0'; z <= y; z++)
 			{
 				putchar(n);
 				putchar('\n');
