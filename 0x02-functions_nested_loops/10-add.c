66c084e9059208c105134c5d88ee4230aa068ce2
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-18 20:42:55 -0700

    Done task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdiio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * @x: entered value
+ * @y: entered value
+ * Return: result
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
