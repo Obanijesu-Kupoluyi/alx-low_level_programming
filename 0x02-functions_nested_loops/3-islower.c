2054242a9911597b12a8942d0170d69d74e2bf93
Obanijesu-Kupoluyi <oluwanimilokan@gmail.com>
2022-10-18 18:51:24 -0700

    Done task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * _islower - Entry point
+ * @c: the value entered
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (c == n)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
